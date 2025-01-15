0bc0eca5b500d53f8ec9136c3251b3cfc031881b
Julius Olson <julius.olsons@gmail.com>
2025-01-15 22:49:56 +0100

    streaming + better setup

--- lua/gpt.lua
+++ lua/gpt.lua
@@ -2,6 +2,7 @@ local http = require("plenary.curl")
 local utils = require("utils")
 
 local state = {
+    open = false,
     prompt = { win = -1, buf = -1 },
     output = { win = -1, buf = -1 },
     conversation = {
@@ -22,11 +23,44 @@ local config = {
 
 
 local function write_data_to_buf(data)
-    data = data or "**This** is some *sample* output"
-
     local lc = vim.api.nvim_buf_line_count(state.output.buf)
     local l = vim.api.nvim_buf_get_lines(state.output.buf, lc - 1, lc, true)
     vim.api.nvim_buf_set_text(state.output.buf, lc - 1, #l[1], -1, -1, vim.split(data or "\n", "\n"))
+end
+
+
+local function streamfun(_, chunk)
+    vim.schedule(function()
+        if chunk == nil or chunk == "" then
+            return
+        end
+
+        local prefix = "data: "
+        local data = string.sub(chunk, #prefix + 1)
+
+        if data == "[DONE]" then
+            return
+        end
+
+        local payload = vim.fn.json_decode(data)
+        local content = payload.choices[1].delta.content
+
+        -- Append content to current chat result
+        if content then
+            state.result = state.result .. content
+        end
+
+        -- Write to buf
+        write_data_to_buf(content)
+        vim.cmd("redraw")
+    end)
+end
+
+
+local function store_answer(_)
+    vim.schedule(function()
+        table.insert(state.conversation, { role = "assistant", content = state.result })
+    end)
 end
 
 local function get_answer(q)
@@ -40,12 +74,15 @@ local function get_answer(q)
         content = q
     })
     local body = {
+        stream = true,
         model = config.model,
         messages = state.conversation,
     }
     local resp = http.post(config.base_url, {
         headers = headers,
         body = vim.fn.json_encode(body),
+        stream = streamfun,
+        callback = store_answer,
     })
     return resp
 end
@@ -75,9 +112,6 @@ local function process_input()
     get_answer(user_input)
 end
 
-local M = {}
-
-
 local function create_bufs()
     state.output.buf = vim.api.nvim_create_buf(false, true)
     vim.api.nvim_buf_set_name(state.output.buf, "AI")
@@ -87,8 +121,34 @@ local function create_bufs()
     vim.api.nvim_buf_set_name(state.prompt.buf, "Prompt")
 end
 
-local function new_flow()
-    create_bufs()
+local M = {}
+
+M.setup = function(opts)
+    opts = opts or {}
+    opts.model = opts.model or config.model
+    opts.output_width = opts.output_width or config.output_width
+    opts.prompt_height = opts.prompt_height or config.prompt_height
+    opts.base_url = opts.base_url or config.base_url
+    config = opts
+end
+
+M.close = function()
+    vim.api.nvim_win_close(state.output.win, false)
+    vim.api.nvim_win_close(state.prompt.win, false)
+    state.open = false
+end
+
+
+M.open = function()
+    if state.open then
+        vim.api.nvim_set_current_win(state.prompt.win)
+        return
+    end
+    state.open = true
+
+    if state.prompt.buf == -1 then
+        create_bufs()
+    end
 
     state.output.win = vim.api.nvim_open_win(state.output.buf, true, { split = "right" })
     vim.api.nvim_win_set_width(state.output.win, config.output_width)
@@ -96,9 +156,9 @@ local function new_flow()
     state.prompt.win = vim.api.nvim_open_win(state.prompt.buf, true, { split = "below" })
     vim.api.nvim_win_set_height(state.prompt.win, config.prompt_height)
 
-    vim.keymap.set("n", "<CR>", function() process_input() end, { buffer = state.prompt.buf })
-end
 
-new_flow()
+    vim.keymap.set("n", "<CR>", function() process_input() end, { buffer = state.prompt.buf })
+    vim.keymap.set("n", "q", function() M.close() end, { buffer = state.prompt.buf })
+end
 
 return M
